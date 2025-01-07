7ac08c0835b340d7ae3990636c140c02e939f03a
Rohith-S-07 <rohithskr01@gmail.com>
2025-01-07 23:43:28 +0530

    dotenv added

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Optional: Increase timeout duration
+      serverSelectionTimeoutMS: 30000,
     });
     console.log('MongoDB Connected');
   } catch (error) {
