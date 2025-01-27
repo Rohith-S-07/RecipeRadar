10350165fbae9186fac98b7bc283e49930ba8539
Rohith-S-07 <rohithskr01@gmail.com>
2025-01-27 11:02:54 +0530

    Backend Hosted

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-  BASE_URL: 'http://localhost:5000/api'
+  // BASE_URL: 'http://localhost:5000/api',
+  BASE_URL: 'https://reciperadarbackend.onrender.com'
 };
 
 export default config;
