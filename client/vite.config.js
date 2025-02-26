71f2def5d7fcd9d7864f298bfddbcf756e899a54
Rohith-S-07 <rohithskr01@gmail.com>
2025-02-26 21:15:46 +0530

    SEO done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://reciperadar-app.onrender.com",
+      routes: ["/", "/about", "/recipes", "/contact"],
+    }),
+  ],
   server: {
     port: process.env.PORT || 5173,
     host: '0.0.0.0',
   },
   define: {
     'process.env': {},
-  }
+  },
 });
