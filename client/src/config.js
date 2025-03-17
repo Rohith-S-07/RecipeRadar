7b9139c52ec0e9a7b4f94e0641ea08225c29a37c
Rohith-S-07 <rohithskr01@gmail.com>
2025-03-18 01:09:14 +0530

    Profile Completed

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  BASE_URL: 'http://localhost:5000',
+  // BASE_URL: 'http://localhost:5000',
   // BASE_URL: 'http://192.168.2.148:5000',
-  // BASE_URL: 'https://reciperadarbackend.onrender.com'
+  BASE_URL: 'https://reciperadarbackend.onrender.com'
 };
 
 export default config;
